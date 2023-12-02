2bcd7b1196fc081ba07395b4b7b7cfde168412b6
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-12-02 16:40:15 +0800

    bottom nav can navigate when clicked on each of the menu items

--- app/src/main/java/LearnBottomNav/LearnBottomNavigation.java
+++ app/src/main/java/LearnBottomNav/LearnBottomNavigation.java
@@ -1,14 +1,20 @@
 package LearnBottomNav;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.techwizards.learningfragment.R;
 
-public class LearnBottomNavigation extends AppCompatActivity {
+public class LearnBottomNavigation extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView bottomNavView;
     private NavController navController;
@@ -17,8 +23,64 @@ public class LearnBottomNavigation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_bottom_navigation);
 
-//        bottomNavView = findViewById(R.id.bottomNavView);
-//        navController = findNav;
+        //init
+        bottomNavView = findViewById(R.id.bottomNavView);
 
+        bottomNavView.setOnNavigationItemSelectedListener(this);
+
+        //default fragment
+        loadFragment(new HomeFragment());
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) { //clicks on every menu item
+
+        Fragment fragment = null;
+
+        int id= item.getItemId();
+
+        if (id == R.id.DestHome) {
+            fragment = new HomeFragment();
+        }
+
+        else if (id== R.id.DestProfile){
+            fragment = new ProfileFragment();
+        }
+
+        else if (id == R.id.DestSettings){
+            fragment = new SettingsFragment();
+        }
+
+        else {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+
+        //load to the screen
+        if (fragment!= null){
+            loadFragment(fragment);
+        }
+
+
+        return true;
+    }
+
+
+    private void loadFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView_bottomNav, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
